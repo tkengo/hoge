ad01ea0b28ffcd7e5377764be4d60e0afae17997
Kengo Tateishi <embrace.ddd.flake.peace@gmail.com>
2014-01-10 22:53:20 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Soleil::Application.config.secret_key_base = '24057bfae514bdee137d53a729da4e5eeef6da730047b202ca669b7d169e4cb2a16880c8e239327db76829fc428e20ef23369b64b252e7f940717cf01d392ae3'
