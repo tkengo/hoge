bfde3092411b7d1dad8b0b5a3f23fb29e4a5df91
Kengo Tateishi <embrace.ddd.flake.peace@gmail.com>
2014-01-11 07:05:58 +0900

    defined routes to view a signin page and do signin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Soleil::Application.routes.draw do
   root 'welcome#index'
 
+  get 'signin' => 'sessions#new', as: :new_signin
+  post 'signin' => 'sessions#create'
   resources :users, only: [ :new, :create ]
 end
