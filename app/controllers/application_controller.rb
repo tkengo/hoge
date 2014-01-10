622a13ed267348bbc777b263452393833c3a8e70
Kengo Tateishi <embrace.ddd.flake.peace@gmail.com>
2014-01-11 06:59:00 +0900

    implemented around login process and defined current_user, logged_in? as a helper method to use it in a view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,23 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in?
+
+  private
+
+  def logged_in?
+    return session.key?(:login_user_id) && !session[:login_user_id].nil?
+  end
+
+  def login(user_id)
+    session[:login_user_id] = user_id
+  end
+
+  def logout
+    session[:login_user_id] = nil
+  end
+
+  def current_user
+    logged_in? ? User.find(session[:login_user_id]) : nil
+  end
 end
