79ea9ea8822555ad3c16c2b3252921556a50a640
Kengo Tateishi <embrace.ddd.flake.peace@gmail.com>
2014-01-15 06:15:46 +0900

    toggle showing and hiding form

--- app/assets/javascripts/views/project_list_view.js
+++ app/assets/javascripts/views/project_list_view.js
@@ -1,8 +1,21 @@
 var ProjectListView = Backbone.View.extend({
   events: {
-    'click #new-project': 'showNewProjectForm'
+    'click #new-project': 'showNewProjectForm',
+    'blur #new-project-name': 'hideNewProjectForm',
+    'keyup #new-project-name': 'createProject'
   },
   showNewProjectForm: function() {
+    $('.js-text-area').hide();
+    $('.js-form-area').show();
+    $('#new-project-name').focus();
+  },
+  hideNewProjectForm: function() {
+    $('.js-text-area').show();
+    $('.js-form-area').hide();
+  },
+  createProject: function(e) {
+    if (e.keyCode == 13) {
+    }
   }
 });
 
